c07e7ce5ddc649f7645fec59c45207af27f2566d
alex <alex@frogrock.net>
2020-07-29 17:00:23 -0700

    new beta build - 35360

--- env-config.js
+++ env-config.js
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     'process.env.URL_PREFIX': isProduction ? '/shadowlands-cheatsheet' : '',
-    'process.env.LAST_UPDATED': 'Sunday, July 26, 2020 8:49:18 PM GMT-07:00 (build 35256)',
+    'process.env.LAST_UPDATED': 'Wednesday, July 29, 2020 4:36:37 PM GMT-07:00 (build 35360)',
 }
