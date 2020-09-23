63fdec21dbac42cb5e3a59efd7cb10404ee3a0b2
alex <alex@frogrock.net>
2020-09-23 16:09:58 -0700

    covenant data

--- env-config.js
+++ env-config.js
@@ -2,5 +2,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
     'process.env.URL_PREFIX': isProduction ? '/shadowlands-cheatsheet' : '',
-    'process.env.LAST_UPDATED': 'Wednesday, July 29, 2020 4:36:37 PM GMT-07:00 (build 35360)',
+    'process.env.LAST_UPDATED': 'Wednesday, September 23, 2020 4:08:37 PM GMT-07:00 (build 35978)',
 }
